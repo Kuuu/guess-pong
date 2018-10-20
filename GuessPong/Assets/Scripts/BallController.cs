39337a56708835e9de1c88dd65a3ae2f8235563f
Ilya Kudinov <i.kudinov2@gmail.com>
2018-10-20 19:59:15 +0300

    Ball movement and collisions

---
+++ GuessPong/Assets/Scripts/BallController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Vector2 currentVelocity;
+    private Vector2 startVelocity;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        startVelocity = new Vector2(200f, 50f);
+        currentVelocity = startVelocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = currentVelocity * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision..");
+
+        if (collision.collider.CompareTag("bat"))
+        {
+            Debug.Log("..Collision with bat");
+            currentVelocity = new Vector2(-currentVelocity.x, currentVelocity.y);
+        } else if (collision.collider.CompareTag("wall"))
+        {
+            Debug.Log("..Collision with wall");
+            currentVelocity = new Vector2(currentVelocity.x, -currentVelocity.y);
+        }
+    }
+}
---
+++ GuessPong/Assets/Scripts/BatController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour {
+
+    public string side = "Left";
+    public float speed = 300f;
+
+    private Rigidbody2D rb;
+    private float movement = 0;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxisRaw(side + "Move");
+	}
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, movement * speed * Time.fixedDeltaTime);
+    }
+}
